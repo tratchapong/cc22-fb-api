8062fd787c8497f3dccfdd246db70df9d4888b06
tratchapong <tratchapong@gmail.com>
2026-03-11 16:59:52 +0700

    graceful shutdown

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,21 +1,29 @@
 import bcrypt from 'bcrypt'
 import { prisma } from '../src/lib/prisma.js'
 
-const hashedPassword = bcrypt.hashSync('123456', 8)
+const hashedPassword = () => bcrypt.hashSync('123456', 8)
 
 const userData = [
- { firstName: 'Andy', lastName: 'Codecamp', password: hashedPassword, email: 'andy@ggg.mail',
-     profileImage: 'https://www.svgrepo.com/show/420364/avatar-male-man.svg' }, 
- { firstName: 'Bobby', lastName: 'Codecamp', password: hashedPassword, email: 'bobby@ggg.mail',
-     profileImage: 'https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg'},
- { firstName: 'Candy', lastName: 'Codecamp', password: hashedPassword, mobile: '1111111111',
-     profileImage: 'https://www.svgrepo.com/show/420327/avatar-child-girl.svg'},
- { firstName: 'Danny', lastName: 'Codecamp', password: hashedPassword, mobile: '2222222222',
-     profileImage: 'https://www.svgrepo.com/show/420314/builder-helmet-worker.svg'},
+  {
+    firstName: 'Andy', lastName: 'Codecamp', password: hashedPassword(), email: 'andy@ggg.mail',
+    profileImage: 'https://www.svgrepo.com/show/420364/avatar-male-man.svg'
+  },
+  {
+    firstName: 'Bobby', lastName: 'Codecamp', password: hashedPassword(), email: 'bobby@ggg.mail',
+    profileImage: 'https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg'
+  },
+  {
+    firstName: 'Candy', lastName: 'Codecamp', password: hashedPassword(), mobile: '1111111111',
+    profileImage: 'https://www.svgrepo.com/show/420327/avatar-child-girl.svg'
+  },
+  {
+    firstName: 'Danny', lastName: 'Codecamp', password: hashedPassword(), mobile: '2222222222',
+    profileImage: 'https://www.svgrepo.com/show/420314/builder-helmet-worker.svg'
+  },
 ]
 
 async function main() {
-  console.log('Clean table...')  
+  console.log('Clean table...')
   await prisma.$transaction([
     prisma.$executeRawUnsafe('SET FOREIGN_KEY_CHECKS = 0;'),
     prisma.$executeRawUnsafe('TRUNCATE TABLE `Like`;'),
@@ -28,15 +36,15 @@ async function main() {
 
   console.log('Start seeding...')
   const createdUsers = await prisma.user.createMany({
-    data : userData,
-    skipDuplicates : true,
+    data: userData,
+    skipDuplicates: true,
   })
   console.log(`Created : ${createdUsers.count} users`)
 }
 
-main().then( async ()=>{
+main().then(async () => {
   await prisma.$disconnect()
-}).catch( async (e)=> {
+}).catch(async (e) => {
   console.error(e)
   await prisma.$disconnect()
   process.exit(1)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import 'dotenv/config'
 import app from './app.js'
+import shutdown from './utils/shutdown.util.js'
 
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, ()=>console.log(`Server on port : ${PORT}`))
+
+process.on('SIGINT', ()=> shutdown('SIGINT') ) // CTRL+C
+process.on('SIGTERM', ()=> shutdown('SIGTERM') ) // kill command
+
+// Catch unhandled errors
+process.on("uncaughtException", ()=>  shutdown('uncaughtException'))
+process.on("unhandledRejection", ()=> shutdown('unhandledRejection'))
