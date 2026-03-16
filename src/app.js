f94fdfe9506324e1d298f03f9d101dcbfd3cac59
tratchapong <tratchapong@gmail.com>
2026-03-16 17:10:07 +0700

    add Cors()

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@ import express from 'express'
 import authRoute from './routes/auth.route.js'
 import notFoundMiddleware from './middlewares/notFound.middleware.js'
 import errorMiddleware from './middlewares/error.middleware.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors({
+  origin: ["http://localhost:5173"], // allowed origins
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // allow cookies if needed
+}))
 app.use(express.json())
 
-app.use('/api/auth', authRoute )
-app.use('/api/post', (req, res)=>{ res.send('post service')})
-app.use('/api/comment',(req, res)=>{ res.send('comment service')})
-app.use('/api/like',(req, res)=>{ res.send('like service')})
+app.use('/api/auth', authRoute)
+app.use('/api/post', (req, res) => { res.send('post service') })
+app.use('/api/comment', (req, res) => { res.send('comment service') })
+app.use('/api/like', (req, res) => { res.send('like service') })
 
 // not found
-app.use( notFoundMiddleware )
+app.use(notFoundMiddleware)
 
 // error middleware
 app.use(errorMiddleware)
